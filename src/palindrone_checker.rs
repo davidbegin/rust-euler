3c0f5df3fe20772c6540113ee7f49c69293a1990
David Begin <davidmichaelbe@gmail.com>
2015-05-07 00:18:52 -0700

    clean up
      I like the overall aesthetics of Rust
      and look forward to explore and experiment with the syntax more

--- src/palindrone_checker.rs
+++ src/palindrone_checker.rs
@@ -10,25 +10,15 @@ pub fn call(num: i32) -> bool {
         if master_value < 1 { break }
     }
 
-    println!("array of digits: {:?}", array_of_digits);
-    println!("length {}", array_of_digits.len());
-
     let odd_eh = array_of_digits.len() % 2 != 0;
-
-    if odd_eh {
-        return false
-    }
+    if odd_eh { return false }
 
     loop {
         if array_of_digits.len() == 0 { break }
         let a = array_of_digits.pop().unwrap();
         let b = array_of_digits.remove(0);
-        println!("a {:?}", a);
-        println!("b {:?}", b);
-
         if a != b { return false }
     }
-
     true
 }
 
