c63a7e9b298503b553ed4763a57f536aa23b8cb9
David Begin <davidmichaelbe@gmail.com>
2015-05-16 22:07:42 -0700

    boy is it ugly, but the tests pass.
     ...then time to clean up this mess

--- src/problem_three/mod.rs
+++ src/problem_three/mod.rs
@@ -6,23 +6,45 @@
 
 pub fn result() {
     println!("\nProblem 3 coming soon!\n");
-    let result = prime_factors(13195);
-    println!("Result: {:?}", result);
 }
 
-fn prime_factors(num: u64) -> Vec<u64> {
-    let mut factors: Vec<u64> = vec![];
-    let mut counter: u64 = 1;
-    let mut remainder: u64;
+fn is_prime(n: u32) -> bool {
+    if n == 2 { return true; }
+    if n < 3 { return false; }
+    if n % 2 == 0 { return false; }
+    let sqrt_limit = ((n as f32).sqrt() as u32);
+    (3..sqrt_limit+1).step_by(2).find(|i| n % i == 0).is_none()
+}
+
+fn prime_factors(num: u32) -> Vec<u32> {
+    let mut factors: Vec<u32> = vec![];
+
+    if num == 0 {
+        return factors;
+    }
+
+    if num == 2 {
+        return vec![2];
+    }
+
+    let mut counter: u32 = 1;
+    let mut remainder: u32;
 
     loop {
-        remainder = num / counter;
-        if remainder == 0 { break }
-        factors.push(remainder);
+        if counter == num { break }
+        remainder = num % counter;
+
+        if remainder == 0 {
+            factors.push(counter);
+        } else {
+            counter += 1;
+            continue
+        }
+
         counter += 1;
     }
 
-    let mut uniq_factors: Vec<u64> = vec![];
+    let mut uniq_factors: Vec<u32> = vec![];
 
     for factor in factors {
         if !uniq_factors.contains(&factor) {
@@ -30,10 +52,10 @@ fn prime_factors(num: u64) -> Vec<u64> {
         }
     }
 
-    let mut prime_factors: Vec<u64> = vec![];
+    let mut prime_factors: Vec<u32> = vec![];
 
     for factor in uniq_factors {
-        if prime_eh(factor) {
+        if is_prime(factor) {
             prime_factors.push(factor);
         }
     }
@@ -41,34 +63,22 @@ fn prime_factors(num: u64) -> Vec<u64> {
     prime_factors
 }
 
-fn all_factors(number_to_find_primes_of: u64) -> Vec<u64> {
-    vec![1, 5, 7, 13, 29, 35, 65, 91, 145, 203, 377, 455, 1015, 1885, 2639, 13195]
-}
-
-fn prime_eh(n: u64) -> bool {
-    if n == 2 { return true; }
-    if n < 3 { return false; }
-    let sqrt_limit = (n as f64).sqrt() as u64;
-    (3..sqrt_limit+1).step_by(2).find(|i| n % i == 0).is_none()
-}
-
 #[cfg(test)]
 mod tests {
     use super::prime_factors;
-    use super::prime_eh;
-    use super::all_factors;
+    use super::is_prime;
 
     #[test]
     fn prime_eh_is_up() {
-        assert_eq!(prime_eh(7), true);
-        assert_eq!(prime_eh(9), false);
-        assert_eq!(prime_eh(11), true);
+        assert_eq!(is_prime(7), true);
+        assert_eq!(is_prime(9), false);
+        assert_eq!(is_prime(11), true);
     }
 
-    // #[test]
-    // fn six_has_some_prime_factors() {
-    //     assert_eq!(prime_factors(6), vec![2, 3]);
-    // }
+    #[test]
+    fn six_has_some_prime_factors() {
+        assert_eq!(prime_factors(6), vec![2, 3]);
+    }
 
     #[test]
     fn two_is_only_prime_factor_of_two() {
@@ -80,32 +90,21 @@ mod tests {
         assert_eq!(prime_factors(2), vec![2]);
     }
 
-    #[test]
-    fn there_are_no_prime_factors_of_zero() {
-        assert_eq!(prime_factors(0), vec![]);
-    }
-
     // #[test]
-    // fn we_have_to_start_somewhere() {
-    //     assert_eq!(prime_factors(13195), [5, 7, 13, 29]);
+    // fn there_are_no_prime_factors_of_zero() {
+    //     assert_eq!(prime_factors(0), vec![]);
     // }
 
     #[test]
-    fn all_factors_is_a_thing() {
-        assert_eq!(
-            all_factors(13195),
-            vec![
-                1, 5, 7, 13, 29, 35, 65, 91,
-                145, 203, 377, 455, 1015, 1885, 2639, 13195
-            ]
-        );
+    fn we_have_to_start_somewhere() {
+        assert_eq!(prime_factors(13195), vec![5, 7, 13, 29]);
     }
 
     #[test]
     fn i_can_tell_if_something_is_prime_from_20_yards_away() {
-        assert!(prime_eh(7));
-        assert!(prime_eh(29));
-        assert!(!prime_eh(9));
-        assert!(!prime_eh(100));
+        assert!(is_prime(7));
+        assert!(is_prime(29));
+        assert!(!is_prime(9));
+        assert!(!is_prime(100));
     }
 }
