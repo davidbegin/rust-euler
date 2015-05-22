91836cf2de0bd295df52bde303b2c7028fa3236e
David Begin <davidmichaelbe@gmail.com>
2015-05-22 00:02:27 -0700

    woah double ** I think I am messing up somewhere
      I also need to start saving the primes somewhere

--- src/primes_primes_primes/mod.rs
+++ src/primes_primes_primes/mod.rs
@@ -20,7 +20,35 @@ pub fn whose_got_the_primes() {
     // although that's probably beyond my skills
 
 
+    example_1();
+}
 
+fn example_1() {
+    let limit: i32 = 121;
+    let range = 2..limit;
+    let first_filtered_vec = return_a_sieve(range.collect());
+    println!("{:?}", first_filtered_vec);
+
+}
+
+fn return_a_sieve(vec_to_be_filtered: Vec<i32>) -> Vec<i32> {
+    let limit = 121;
+
+    let prime: i32 = vec_to_be_filtered[0].clone();
+
+    let non_primes_to_delete = (prime..limit).step_by(prime).collect::<Vec<i32>>();
+
+    let filtered_list = vec_to_be_filtered.iter().filter(|i| {
+        match non_primes_to_delete.iter().find(|&x| { *x == **i }) {
+            Some(_) => false,
+            None => true
+        }
+    });
+
+    filtered_list.map(|i| *i).collect::<Vec<i32>>()
+}
+
+fn messy_notes() {
     // Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
 
     let n: i32 = 121;
@@ -40,12 +68,23 @@ pub fn whose_got_the_primes() {
         }
     });
 
-    println!("filtered_list: {:?}", filtered_list.collect::<Vec<_>>());
+    // println!("filtered_list: {:?}", filtered_list.collect::<Vec<_>>());
     // now I need to make a filtered vec
 
     // and mark them in the list (these will be 2p, 3p, 4p, ... ; the p itself should not be marked).
 
     // Find the first number greater than p in the list that is not marked.
+
+    let next_number: i32 = filtered_list.collect::<Vec<_>>()[0];
+
+    println!("next number: {}", next_number);
+
+    // so I need a function that takes an array, and a prime number and returns a new vec
+    //
+    // and then you use that array to filter the next
+    //
+    // so then you actually just need to send in an array that filters based on the lowest number
+
     // If there was no such number, stop. Otherwise,
     // let p now equal this new number (which is the next prime), and repeat from step 3.
 }
