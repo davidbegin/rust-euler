edda7763097a50e3ecbf2be1df7ad132f14fde3e
David Begin <davidmichaelbe@gmail.com>
2015-05-22 00:12:18 -0700

    a very dumb struct to hold some state

--- src/primes_primes_primes/mod.rs
+++ src/primes_primes_primes/mod.rs
@@ -20,14 +20,34 @@ pub fn whose_got_the_primes() {
     // although that's probably beyond my skills
 
 
-    example_1();
+    // example_1();
+    exploration_of_marked_num_struct();
+}
+
+struct NumberThatCanBeMarked {
+    marked: bool
+}
+
+fn exploration_of_marked_num_struct() {
+    let durr = NumberThatCanBeMarked { marked: false };
+    let range: Vec<NumberThatCanBeMarked> = vec![durr];
+
+    println!("range {:?}", range.iter().map(|i| i.marked).collect::<Vec<bool>>());
 }
 
 fn example_1() {
+
+    // ahh I need to start marking!
+    //
+    // so I need a vec of an object
     let limit: i32 = 121;
     let range = 2..limit;
     let first_filtered_vec = return_a_sieve(range.collect());
-    println!("{:?}", first_filtered_vec);
+    let second_filtered_vec = return_a_sieve(first_filtered_vec);
+    let third_filtered_vec = return_a_sieve(second_filtered_vec);
+    let fourth_filtered_vec = return_a_sieve(third_filtered_vec);
+    let fifth_filtered_vec = return_a_sieve(fourth_filtered_vec);
+    // println!("{:?}", fifth_filtered_vec);
 
 }
 
@@ -51,40 +71,12 @@ fn return_a_sieve(vec_to_be_filtered: Vec<i32>) -> Vec<i32> {
 fn messy_notes() {
     // Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
 
-    let n: i32 = 121;
-    let range = 2..n;
-
     // Initially, let p equal 2, the first prime number.
-
-    let p: i32 = 2;
-
     // Starting from p, enumerate its multiples by counting to n in increments of p,
-    let ps_to_delete = (p..n).step_by(p).collect::<Vec<i32>>();
-
-    let filtered_list = range.filter(|i| {
-        match ps_to_delete.iter().find(|&x| { *x == *i }) {
-            Some(_) => false,
-            None => true
-        }
-    });
-
-    // println!("filtered_list: {:?}", filtered_list.collect::<Vec<_>>());
-    // now I need to make a filtered vec
 
     // and mark them in the list (these will be 2p, 3p, 4p, ... ; the p itself should not be marked).
 
     // Find the first number greater than p in the list that is not marked.
-
-    let next_number: i32 = filtered_list.collect::<Vec<_>>()[0];
-
-    println!("next number: {}", next_number);
-
-    // so I need a function that takes an array, and a prime number and returns a new vec
-    //
-    // and then you use that array to filter the next
-    //
-    // so then you actually just need to send in an array that filters based on the lowest number
-
     // If there was no such number, stop. Otherwise,
     // let p now equal this new number (which is the next prime), and repeat from step 3.
 }
